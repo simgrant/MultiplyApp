4bc63aad4b179d41c1abcf2e64dcc1c45b5912de
simone <simthecode@gmail.com>
2025-01-08 18:34:00 -0500

    adding assets and ui vc

---
+++ MultiplyApp/MultiplyApp/ColorView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorView.swift
+//  MultiplyApp
+//
+//  Created by Simone  on 1/8/25.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorView()
+}
