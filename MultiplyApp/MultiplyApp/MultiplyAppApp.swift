62f5ce73cce4ab51667d297c341d4a92106f4a86
simone <simthecode@gmail.com>
2025-01-07 19:10:24 -0500

    initial commit

---
+++ MultiplyApp/MultiplyApp/MultiplyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyAppApp.swift
+//  MultiplyApp
+//
+//  Created by Simone  on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
